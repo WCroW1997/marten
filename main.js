98c474396308350b67699e9ecd5bb07a33d1cb92
WCroW1997 <wcrow1997@gmail.com>
2024-07-22 16:09:36 +0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+let menuBtn = document.querySelector('.header__burger');
+let menu = document.querySelector('.nav');
+let lock = document.querySelector('body');
+
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    lock.classList.toggle('lock');
+})
+
 var swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
     grid: {
         rows: 2,
     },
@@ -19,4 +28,27 @@ var swiper = new Swiper(".swiper", {
     scrollbar: {
         el: '.swiper-scrollbar',
     },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            grid: {
+                rows: 1,
+            },
+            slidesPerView: 1,
+        },
+
+        640: {
+            slidesPerView: 2,
+
+        }
+        ,
+        800: {
+            grid: {
+                rows: 2,
+            },
+            slidesPerView: 3,
+        }
+
+    }
 });
